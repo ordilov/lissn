fa9778fbe322b71b1ddeeca83552d977e066d943
ordilov <ordilov@gmail.com>
2022-01-26 02:27:56 +0900

    Updates

---
+++ _next/static/JWzkTvDdWnchHelAexUlz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fcb1608f2.115eb6597d28444ce3ed.js","static\u002Fchunks\u002Fpages\u002Findex-340b03a193e2014d45d7.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-dd44c2484321e9793cbf.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
