8cae99151e51763f92ba0302d44e3105eb522394
ordilov <ordilov@gmail.com>
2022-01-26 03:05:42 +0900

    Updates

--- _next/static/Zx3RwOENCseuFe3cf1WGP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/cb1608f2-3b9b1a8a0fbb2553.js","static/chunks/1-aa6a35bd9c188493.js","static/chunks/pages/index-72d1ab9ba4a87e96.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
